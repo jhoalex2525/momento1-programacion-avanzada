fbaabdee88629081b919d77b528b6bc0d4fc3d65
jhonatan.ossa <jhonatan.ossa@ceiba.com.co>
2022-03-18 07:33:36 -0500

    Se realiza el problema dos taller uno

---
+++ taller1/problema2.js
@@ -0,0 +1,13 @@
+// 2.PROBLEMA: Obi-Wan Kenobi, necesita clasificar diferentes códigos de
+// acceso a las naves de guerra los cuáles reposan en la base de datos
+// central, para ello debe programar una función que permita recibir la
+// palabra clave de cada nave y separar imprimiendo solo el nombre del
+// piloto asignado a cada nave.
+// Tenga en cuenta que el formato de todos los datos es el siguiente:
+// • ARQ2555: Sara Bel-Sun
+// • ARQ2556: Nodin Chavdri
+// • ARQ2557: Finn
+
+// funcion flecha para separar codigos con split
+let separarCodigo=codigo => "El nombre es "+codigo.split(": ")[1]
+console.log(separarCodigo("ARQ2555: Sara Bel-Sun"))

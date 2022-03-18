a5ee59eb3e96c0acf291e7be086c248dbf79dcd6
jhonatan.ossa <jhonatan.ossa@ceiba.com.co>
2022-03-18 07:29:28 -0500

    Se realiza el problema uno taller uno

---
+++ taller1/problema1.js
@@ -0,0 +1,11 @@
+// 1.PROBLEMA: Yoda el maestro jedi, necesita enseñar a sus aprendices
+// Padawan a calcular la distancia a la que cualquiera de los planetas de la
+// ruta N-14 pertenecientes a la ruta comercial de la federación
+// intergaláctica, se encuentra del planeta NABOO.
+// Para calcular dicha distancia se debe:
+// • Recibir las coordenadas X,Y del planeta en UA
+// • Establecer la distancia como:
+
+// Función flecha para calcular la distancia entre dos planetas
+let distanciaPlanetasFlecha = (x1,y1,x2,y2) => "La distancia entre los planetas es "+ Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2)).toFixed(2)
+console.log(distanciaPlanetasFlecha(0,0,-10,-10))

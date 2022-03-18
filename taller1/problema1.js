7372a0c3a0fe8d2e07eebf4f17dbfdc64f35810d
jhonatan.ossa <jhonatan.ossa@ceiba.com.co>
2022-03-18 07:56:33 -0500

    se realizan cambios en problema uno en taller uno

--- taller1/problema1.js
+++ taller1/problema1.js
@@ -6,6 +6,6 @@
 // • Recibir las coordenadas X,Y del planeta en UA
 // • Establecer la distancia como:
 
-// Función flecha para calcular la distancia entre dos planetas
-let distanciaPlanetasFlecha = (x1,y1,x2,y2) => "La distancia entre los planetas es "+ Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2)).toFixed(2)
-console.log(distanciaPlanetasFlecha(0,0,-10,-10))
+// función flecha para calcular la distancia entre dos planetas
+let distanciaPlanetas = (x1,y1,x2,y2) => "La distancia entre los planetas es "+ Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2)).toFixed(2)
+console.log(distanciaPlanetas(0,0,-10,-10))
